36a7af9e718157c9e3c78ad6e7413f74a0cd79d0
goldentoaste <rayg2375@gmail.com>
2023-11-14 22:03:34 -0700

    abab

---
+++ alphaBetaPlayer.py
@@ -0,0 +1,20 @@
+
+
+from player import Player
+from board import Board, BLACK, EMPTY, WHITE
+
+
+class AlphaBetaPlayer(Player):
+    
+    def __init__(self, board: Board):
+        super(board)
+        
+    def genMove(self, color):
+        # TODO do the alpha beta thing here
+        return super().genMove(color)
+    
+    
+    def alphaBeta(self, board, alpha, beta):
+        
+        
+        return
---
+++ player.py
@@ -0,0 +1,15 @@
+
+
+from board import Board
+
+
+class Player:
+    def __init__(self, board: Board):
+        self.board = board
+    
+    def genMove(self, color):
+        # given color, return a move this player should play
+        return (0,0)
+    
+    def replaceBoard(self, board: Board):
+        self.board = board
